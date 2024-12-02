16ac8e3e74eabe0b0cf01ecb6e2a22f25313834f
Brendan Hoag <Realbrendanhoag@gmail.com>
2024-12-02 18:38:16 -0500

    Autoencoder #1 done

--- sample_ae.py
+++ sample_ae.py
@@ -32,7 +32,7 @@ class Sample_AE(_Autoencoder):
                 nn.Dropout(p=0.2),
                 nn.Linear(256, 16),
                 nn.ReLU(),
-            ]
+            ],
         )
 
         self.decoder = Model(
@@ -43,47 +43,46 @@ class Sample_AE(_Autoencoder):
                 nn.Linear(256, n_kernels * 8 * 8),
                 nn.ReLU(),
                 nn.Unflatten(1, (n_kernels, 8, 8)),
-                nn.ConvTranspose2d(n_kernels, n_kernels, kernel_size=3, stride=2,
-                                   padding=1, output_padding=1),
+                nn.ConvTranspose2d(
+                    n_kernels, n_kernels, kernel_size=3, stride=2, padding=1, output_padding=1
+                ),
                 nn.ReLU(),
-                nn.ConvTranspose2d(n_kernels, n_kernels, kernel_size=3, stride=2,
-                                   padding=1, output_padding=1),
+                nn.ConvTranspose2d(
+                    n_kernels, n_kernels, kernel_size=3, stride=2, padding=1, output_padding=1
+                ),
                 nn.ReLU(),
-                nn.ConvTranspose2d(n_kernels, 3, kernel_size=3, stride=2,
-                                   padding=1, output_padding=1),
+                nn.ConvTranspose2d(
+                    n_kernels, 3, kernel_size=3, stride=2, padding=1, output_padding=1
+                ),
                 nn.Sigmoid(),
-            ]
+            ],
         )
 
         self.model = Model(
-            input_shape=self.encoder.input_shape,
-            layers=[
-                self.encoder,
-                self.decoder
-            ]
+            input_shape=self.encoder.input_shape, layers=[self.encoder, self.decoder]
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     epochs = int(sys.argv[1]) if len(sys.argv) > 1 else None
 
-    data = Data.load('data', image_size=64)
+    data = Data.load("data", image_size=64)
     data.shuffle()
 
-    ae = Sample_AE('models/sample_ae.pt')
+    ae = Sample_AE("models/sample_ae.pt")
     ae.print()
 
     if not epochs:
-        print(f'\nLoading {ae.path}...')
+        print(f"\nLoading {ae.path}...")
         ae.load()
     else:
-        print(f'\nTraining...')
+        print(f"\nTraining...")
         ae.train(epochs, data)
-        print(f'\nSaving {ae.path}...')
+        print(f"\nSaving {ae.path}...")
         ae.save()
 
-    print(f'\nGenerating samples...')
+    print(f"\nGenerating samples...")
     samples = ae.generate(data)
     data.display(32)
     samples.display(32)
