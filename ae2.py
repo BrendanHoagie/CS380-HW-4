48ede884ed2b7fb1d6a0e3b6acc289bde724bebe
Brendan Hoag <Realbrendanhoag@gmail.com>
2024-12-03 16:13:28 -0500

    AE2 workable

--- ae2.py
+++ ae2.py
@@ -4,10 +4,11 @@ import torch.nn as nn
 from autoencoder import _Autoencoder
 from data import Data
 from model import Model
+from ae1 import AE1
 
 
-class AE1(_Autoencoder):
-    """AutoEncoder 1 - single convolution + max-pooling + single deconvolution w/ sigmoid"""
+class AE2(_Autoencoder):
+    """AutoEncoder 2 - single convolution + max-pooling + single deconvolution w/ ReLu"""
 
     def __init__(self, path):
         super().__init__(path)
@@ -15,11 +16,10 @@ class AE1(_Autoencoder):
         n_kernels = 64
 
         self.encoder = Model(
-            # input_size, output_size, num_params, kernel size
-            input_shape=(self.BATCH_SIZE, 3, 64, 64),
+            input_shape=(self.BATCH_SIZE, 64, 16, 32),
             layers=[
                 nn.Conv2d(
-                    in_channels=3,
+                    in_channels=64,
                     out_channels=n_kernels,
                     kernel_size=3,
                     padding=1,
@@ -30,17 +30,17 @@ class AE1(_Autoencoder):
         )
 
         self.decoder = Model(
-            input_shape=(self.BATCH_SIZE, 64, 16, 32),
+            input_shape=(self.BATCH_SIZE, 64, 32, 16),
             layers=[
                 nn.ConvTranspose2d(
                     in_channels=n_kernels,
-                    out_channels=3,
+                    out_channels=64,
                     kernel_size=3,
                     stride=2,
                     padding=1,
                     output_padding=1,
                 ),
-                nn.Sigmoid(),
+                nn.ReLU(),
             ],
         )
 
@@ -56,19 +56,23 @@ if __name__ == "__main__":
     data = Data.load("data", image_size=64)
     data.shuffle()
 
-    ae = AE1("models/ae1.pt")
-    ae.print()
+    ae1 = AE1("models/ae1.pt")
+    ae1.load()
+    data_thru_ae1 = ae1.encode(data)
+
+    ae2 = AE2("models/ae2.pt")
+    ae2.print()
 
     if not epochs:
-        print(f"\nLoading {ae.path}...")
-        ae.load()
+        print(f"\nLoading {ae2.path}...")
+        ae2.load()
     else:
         print(f"\nTraining...")
-        ae.train(epochs, data)
-        print(f"\nSaving {ae.path}...")
-        ae.save()
+        ae2.train(epochs, data_thru_ae1)
+        print(f"\nSaving {ae2.path}...")
+        ae2.save()
 
     print(f"\nGenerating samples...")
-    samples = ae.generate(data)
-    data.display(32)
+    samples = ae2.generate(data_thru_ae1)
+    data_thru_ae1.display(32)
     samples.display(32)
